9b99f9cc42bb5a38b00591191f10a46c072dc913
JIREHJ <jirehjonathan7@gmail.com>
2025-03-19 22:10:27 -1200

    merge update

--- main.c
+++ main.c
@@ -1,35 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+int main()
+{
+    /*float physics_assignment, physics_coursework, physics_midterm, physics_endterm;
+    float chemistry_assignment, chemistry_coursework, chemistry_midterm, chemistry_endterm;
+    float math_assignment, math_coursework, math_midterm, math_endterm;
 
-void merge(int a[], int m, int b[], int n, int c[]){
 
-    for(int i = 0; i < m; i++){
-        c[i] = a[i];
-    }
-    for (int j = 0; j < n; j++){
-        c[m + j] = b[j];
-    }
+    float avg_physics, avg_chemistry, avg_math;
+
+
+    printf("Enter marks for Physics:\n");
+    printf("Assignment: ");
+   scanf_s("%f", &physics_assignment);
+    printf("Coursework: ");
+    scanf_s("%f", &physics_coursework);
+    printf("Midterm: ");
+    scanf_s("%f", &physics_midterm);
+    printf("End of Term Exam: ");
+   scanf_s("%f", &physics_endterm);
+
+
+    printf("\nEnter marks for Chemistry:\n");
+    printf("Assignment: ");
+   scanf_s("%f", &chemistry_assignment);
+    printf("Coursework: ");
+    scanf_s("%f", &chemistry_coursework);
+    printf("Midterm: ");
+    scanf_s("%f", &chemistry_midterm);
+    printf("End of Term Exam: ");
+    scanf("%f", &chemistry_endterm);
+
+
+    printf("\nEnter marks for Math:\n");
+    printf("Assignment: ");
+  scanf_s("%f", &math_assignment);
+    printf("Coursework: ");
+     scanf_s("%f", &math_coursework);
+    printf("Midterm: ");
+   scanf_s("%f", &math_midterm);
+    printf("End of Term Exam: ");
+     scanf_s("%f", &math_endterm);
+
+
+    avg_physics = (physics_assignment + physics_coursework + physics_midterm + physics_endterm) / 4;
+    avg_chemistry = (chemistry_assignment + chemistry_coursework + chemistry_midterm + chemistry_endterm) / 4;
+    avg_math = (math_assignment + math_coursework + math_midterm + math_endterm) / 4;
+
+
+    printf("\nAverage marks for Physics: %.2f\n", avg_physics);
+    printf("Average marks for Chemistry: %.2f\n", avg_chemistry);
+    printf("Average marks for Math: %.2f\n", avg_math);
+
+
+    float overall_avg = (avg_physics + avg_chemistry + avg_math) / 3;
+    printf("\nOverall average marks: %.2f\n", overall_avg);*/
+
+int n, x;
+printf("Enter the number of domestic animals: ");
+scanf_s("%d", &n);
+
+char domestic_animals[n][50];
+for(int i=0; i<n; i++){
+    printf("Enter the domestic animal %d:", i+1);
+    scanf_s("%s", domestic_animals[i]);
 }
+ printf("Enter the number of wild animals: ");
+scanf_s("%d", &x);
 
-int main(){
-    int a[] = {1,2,3};
-    int b[] = {7,8,9};
-
-    int m = sizeof(a) / sizeof(a[0]);
-    int n = sizeof(b) / sizeof(b[0]);
-
-    int c [m + n];
-
-    merge(a,m,b,n,c);
-    printf("merged array: ");
-    for(int i = 0; i < m + n; i++){
-        printf("%d", c[i]);
+char wild_animals[x][50];
+for(int i=0; i<x; i++){
+    printf("Enter the domestic animal %d:", i+1);
+    scanf_s("%s", wild_animals[i]);
+}
+   /* printf("All the animals:\n ");
+    for(int i=0; i<n; i++){
+        printf("%d. %s\n", i+1, domestic_animals[i]);
     }
-
+    for(int i=0; i<x; i++){
+        printf("%d. %s\n", n+i+1, wild_animals[i]);
+    }*/
+    //merge
+    int size=n+x;
+    char merged_array[size][50];
+    for(int i=0; i<size; i++){
+        strcpy(merged_array[i],domestic_animals[i]);
+    }
+     for(int i=0; i<size; i++){
+        strcpy(merged_array[i+n],wild_animals[i]);
+    }
+    for(int i=0; i<size; i++){
+        printf("%s ", merged_array[i]);
+    }
     return 0;
-
-
 }
-
-
-
